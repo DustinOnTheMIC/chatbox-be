2a67b5ce1ea07c48a93c430126f13baf102a294d
DESKTOP-6KSAIBM\AD <buiductinwork@gmail.com>
2021-11-02 14:25:29 +0700

    change to use json.parse

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -15,8 +15,14 @@ export class AppController {
     return this.appService.getRoomList();
   }
 
+  // @Post('login')
+  // handleLogin(@Body('userName') userName: string, @Body('password') password: string) {
+  //   return this.appService.login(userName, password);
+  // }
+
   @Post('login')
-  handleLogin(@Body('userName') userName: string, @Body('password') password: string) {
+  handleLogin(@Body() body: any) {
+    const {userName, password} = JSON.parse(body.data)
     return this.appService.login(userName, password);
   }
 }
