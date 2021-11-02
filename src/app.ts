43d5210b0c532ca3012f9cd2162343013fe3173c
DESKTOP-6KSAIBM\AD <buiductinwork@gmail.com>
2021-11-02 15:38:28 +0700

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 import { AppModule } from './app.module';
-import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(3005);
 }
 bootstrap();
