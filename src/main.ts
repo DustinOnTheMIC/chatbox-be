b1a07e1e3ffb3bb7b8f0f792cdd0f074ac61ded4
DESKTOP-6KSAIBM\AD <buiductinwork@gmail.com>
2021-11-02 15:21:02 +0700

    fix cors

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {cors: false});
   app.enableCors({
-    origin: 'https://ductin-chatbox.vercel.app/',
+    origin: '*',
     methods: 'GET, PUT, POST, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
   });
